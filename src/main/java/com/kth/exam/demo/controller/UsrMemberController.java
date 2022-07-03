d37a535414c606ccfaf3444abc55ed755ad1bd61
chuchu02 <rlaxogns73@gmail.com>
2022-07-03 23:41:15 +0900

    회원가입시, 중복 로그인아이디 체크

--- src/main/java/com/kth/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kth/exam/demo/controller/UsrMemberController.java
@@ -9,21 +9,24 @@ import com.kth.exam.demo.vo.Member;
 
 @Controller
 public class UsrMemberController {
-private MemberService memberService;
-	
+	private MemberService memberService;
+
 	public UsrMemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		
+
+		if (id == -1) {
+			return "해당 로그인아이디는 이미 사용중입니다.";
+		}
+
 		Member member = memberService.getMemberById(id);
-		
+
 		return member;
 	}
 }
-
