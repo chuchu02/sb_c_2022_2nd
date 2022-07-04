0993666506b8d1582dcf868e30f12c9eef47fe62
chuchu02 <rlaxogns73@gmail.com>
2022-07-05 01:45:09 +0900

    UsrMemberController, MemberService 에 ResultData 제너릭 적용

--- src/main/java/com/kth/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kth/exam/demo/controller/UsrMemberController.java
@@ -19,7 +19,7 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public ResultData<Integer> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
 		if (Ut.empty(loginId)) {
 			return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
@@ -45,10 +45,10 @@ public class UsrMemberController {
 			return ResultData.from("F-6", "email(을)를 입력해주세요.");
 		}
 
-		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
 		if (joinRd.isFail()) {
-			return joinRd;
+			return (ResultData)joinRd;
 		}
 
 		Member member = memberService.getMemberById((int) joinRd.getData1());
