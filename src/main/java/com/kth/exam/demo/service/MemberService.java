34a31606dd1ffc928e610a1053e975f1c6d98d25
chuchu02 <rlaxogns73@gmail.com>
2022-07-11 19:02:37 +0900

    ResultData에 data1Name 추가, 게시물 수정시 권한체크

--- src/main/java/com/kth/exam/demo/service/MemberService.java
+++ src/main/java/com/kth/exam/demo/service/MemberService.java
@@ -15,25 +15,26 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public ResultData<Integer> join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public ResultData<Integer> join(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
 		// 로그인아이디 중복체크
 		Member oldMember = getMemberByLoginId(loginId);
 
 		if (oldMember != null) {
 			return ResultData.from("F-7", Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.", loginId));
 		}
-		
+
 		// 이름+이메일 중복체크
 		oldMember = getMemberByNameAndEmail(name, email);
 
 		if (oldMember != null) {
 			return ResultData.from("F-8", Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email));
 		}
-		
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		int id = memberRepository.getLastInsertId();
-		
-		return ResultData.from("S-1", "회원가입이 완료되었습니다.", id);
+
+		return ResultData.from("S-1", "회원가입이 완료되었습니다.", "id", id);
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
